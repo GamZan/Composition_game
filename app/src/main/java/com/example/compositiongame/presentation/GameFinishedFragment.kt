0b28545322542dc1f7e97b08f359d582b0884061
GamZan <ivangunaza@gmail.com>
2022-08-16 17:08:30 +0300

    Add visual and view binding

---
+++ app/src/main/java/com/example/compositiongame/presentation/GameFinishedFragment.kt
@@ -0,0 +1,30 @@
+package com.example.compositiongame.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.compositiongame.databinding.FragmentGameFinishBinding
+import java.lang.RuntimeException
+
+class GameFinishedFragment:Fragment() {
+
+    private var _binding: FragmentGameFinishBinding? = null
+    private val binding: FragmentGameFinishBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameFinishBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
